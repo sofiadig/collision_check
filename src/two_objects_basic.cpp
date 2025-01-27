149ca967eec7c4f4f6ce1f3071b706f875bba53a
Sofia Dimakis-Groev <sofia.dimakis@hotmail.com>
2025-01-27 05:31:55 +0100

    Added collision checking. Compilation incomplete

--- src/two_objects_basic.cpp
+++ src/two_objects_basic.cpp
@@ -1,5 +1,8 @@
 #include <ros/ros.h>
 #include <geometry_msgs/Pose.h>
+#include "interactivity/interactive_robot.h"
+#include "interactivity/pose_string.h"
+//src/moveit_tutorials/doc/interactivity/include/interactivity/pose_string.h
 // MoveIt
 #include <moveit_msgs/PlanningScene.h>
 #include <moveit_msgs/GetStateValidity.h>
@@ -7,16 +10,83 @@
 #include <moveit_msgs/ApplyPlanningScene.h>
 
 #include <moveit/robot_model_loader/robot_model_loader.h>
+#include <moveit/robot_model/robot_model.h>
 #include <moveit/robot_state/robot_state.h>
 #include <moveit/robot_state/conversions.h>
-
+#include <moveit/planning_scene/planning_scene.h>
+#include <moveit/collision_detection/collision_tools.h>
 #include <moveit_visual_tools/moveit_visual_tools.h>
 
-// bool stateFeasibilityTestExample(const moveit::core::RobotState& kinematic_state, bool /*verbose*/)
-// {
-//   const double* joint_values = kinematic_state.getJointPositions("panda_joint1");
-//   return (joint_values[0] > 0.0);
-// }
+planning_scene::PlanningScene* g_planning_scene = nullptr;
+ros::Publisher* g_marker_array_publisher = nullptr;
+visualization_msgs::MarkerArray g_collision_points;
+
+void publishMarkers(visualization_msgs::MarkerArray& markers)
+{
+  // delete old markers
+  if (!g_collision_points.markers.empty())
+  {
+    for (auto& marker : g_collision_points.markers)
+      marker.action = visualization_msgs::Marker::DELETE;
+
+    g_marker_array_publisher->publish(g_collision_points);
+  }
+
+  // move new markers into g_collision_points
+  std::swap(g_collision_points.markers, markers.markers);
+
+  // draw new markers (if there are any)
+  if (!g_collision_points.markers.empty())
+    g_marker_array_publisher->publish(g_collision_points);
+}
+
+void computeCollisionContactPoints(InteractiveRobot& robot)
+{
+  g_planning_scene->getWorldNonConst();
+
+  // Collision Requests
+  collision_detection::CollisionRequest c_req;
+  collision_detection::CollisionResult c_res;
+  c_req.group_name = robot.getGroupName();
+  c_req.contacts = true;
+  c_req.max_contacts = 100;
+  c_req.max_contacts_per_pair = 5;
+  c_req.verbose = false;
+
+  // Checking for Collisions
+  g_planning_scene->checkCollision(c_req, c_res, *robot.robotState());
+
+  if (c_res.collision)
+  {
+    ROS_INFO_STREAM("COLLIDING contact_point_count: " << c_res.contact_count);
+    if (c_res.contact_count > 0)
+    {
+      std_msgs::ColorRGBA color;
+      color.r = 1.0;
+      color.g = 0.0;
+      color.b = 1.0;
+      color.a = 0.5;
+      visualization_msgs::MarkerArray markers;
+
+      /* Get the contact points and display them as markers */
+      collision_detection::getCollisionMarkersFromContacts(markers, "panda_link0", c_res.contacts, color,
+                                                           ros::Duration(),  // remain until deleted
+                                                           0.01);            // radius
+      publishMarkers(markers);
+    }
+  }
+
+  else
+  {
+    ROS_INFO("Not colliding");
+
+    // delete the old collision point markers
+    visualization_msgs::MarkerArray empty_marker_array;
+    publishMarkers(empty_marker_array);
+  }
+}
+
+
 
 int main(int argc, char** argv)
 {
@@ -26,6 +96,9 @@ int main(int argc, char** argv)
   spinner.start();
 
   ros::NodeHandle node_handle;
+
+  InteractiveRobot robot;
+  g_planning_scene = new planning_scene::PlanningScene(robot.robotModel());
 
   moveit_visual_tools::MoveItVisualTools visual_tools("panda_link0");
   visual_tools.deleteAllMarkers();
@@ -42,7 +115,7 @@ int main(int argc, char** argv)
   }
   visual_tools.prompt("Press 'next' in the RvizVisualToolsGui window to start the demo");
 
-  // ---------------------------------
+  // --------------Add first object: Box -------------------
   moveit_msgs::AttachedCollisionObject box;
   box.link_name = "panda_hand";
   box.object.header.frame_id = "panda_hand";
@@ -63,14 +136,58 @@ int main(int argc, char** argv)
   box.object.primitive_poses.push_back(pose);
   box.object.operation = box.object.ADD;
 
-  ROS_INFO("Adding the object into the world at the location of the hand.");
-  moveit_msgs::PlanningScene planning_scene;
-  planning_scene.world.collision_objects.push_back(box.object);
-  planning_scene.is_diff = true;
-  planning_scene_diff_publisher.publish(planning_scene);
+  ROS_INFO("Adding the first object into the world at the location of the hand.");
+  moveit_msgs::PlanningScene m_planning_scene;
+  m_planning_scene.world.collision_objects.push_back(box.object);
+  m_planning_scene.is_diff = true;
+  planning_scene_diff_publisher.publish(m_planning_scene);
   visual_tools.prompt("Press 'next' in the RvizVisualToolsGui window to continue the demo");
 
+  // --------------Add second object: Cylinder -------------------
 
+  moveit_msgs::AttachedCollisionObject cylinder;
+  cylinder.link_name = "panda_hand";
+  cylinder.object.header.frame_id = "panda_link0";
+  cylinder.object.id = "cylinder";
+
+  geometry_msgs::Pose pose2;
+  pose2.position.x = 0.3;
+  pose2.position.y = -0.5;
+  pose2.position.z = 0.5;
+  pose2.orientation.w = 1.0;
+
+  shape_msgs::SolidPrimitive primitive2;
+  primitive2.type = primitive2.CYLINDER;
+  primitive2.dimensions.resize(3);
+  primitive2.dimensions[0] = 0.4;
+  primitive2.dimensions[1] = 0.03;
+
+  cylinder.object.primitives.push_back(primitive2);
+  cylinder.object.primitive_poses.push_back(pose2);
+  cylinder.object.operation = cylinder.object.ADD;
+
+  ROS_INFO("Adding the second object into the world at the location of the hand.");
+  //moveit_msgs::PlanningScene m_planning_scene;
+  m_planning_scene.world.collision_objects.push_back(cylinder.object);
+  m_planning_scene.is_diff = true;
+  planning_scene_diff_publisher.publish(m_planning_scene);
+  visual_tools.prompt("Press 'next' in the RvizVisualToolsGui window to continue the demo");
+
+//################################################################################
+
+  ROS_INFO("Starting collision detection.");
+  //InteractiveRobot robot;
+  g_planning_scene->usePlanningSceneMsg(m_planning_scene);
+
+  // Create a marker array publisher for publishing contact points
+  g_marker_array_publisher =
+      new ros::Publisher(node_handle.advertise<visualization_msgs::MarkerArray>("interactive_robot_marray", 100));
+
+  robot.setUserCallback(computeCollisionContactPoints);
+
+  ros::spin();
+  delete g_planning_scene;
+  delete g_marker_array_publisher;
   ros::shutdown();
   return 0;
 }
